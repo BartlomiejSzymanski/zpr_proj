94bea7c17405acec1e554402ea1db4e5de0d2381
Bartlomiej Szymanski <bartek.szymanski9204@gmail.com>
2023-06-06 22:53:24 +0000

    finished web interface

--- web_interface/main_menu/static/js/board.js
+++ web_interface/main_menu/static/js/board.js
@@ -1,6 +1,16 @@
-function createBoard(boardSize) {
+/**
+ * Creates board dynamically based on board_size set in board.html
+ */
+function createBoard() {
   const board = document.getElementById("board");
+  boardSize = board.dataset.boardSize;
+  board.dataset.gameMode = document.getElementById("mode").getAttribute("data-game-mode");
+  
+  // Set CSS custom property
   board.style.setProperty("--board-size", boardSize); 
+  
+  console.log(`GAME MODE: ${board.dataset.gameMode}`)
+
   board.selectedField = null; 
   board.currentPlayer = 1; 
   board.highlightedCell = null; 
@@ -34,7 +44,11 @@ function createBoard(boardSize) {
   }
 }
 
-
+/**
+ * 
+ * @param {HTMLElement} subCell subcell of a Cell that has been selected
+ * @param {HTMLElement} board current board which contains selectedField
+ */
 function handleClick(subCell, board) {
   if (board.selectedField === subCell) {
     board.selectedField.classList.remove("selected"); 
@@ -48,12 +62,34 @@ function handleClick(subCell, board) {
   }
 
 }
-
-function makeMove(cell, subRow, subCol, currentPlayer){
-  selectedField = getSubcell(cell,subRow, subCol);
+/**
+ * Sets a sign corresponding to player id to a given field of a subcell in a given cell
+ * @param {HTMLElement} targetCell cell to which the move will be made
+ * @param {number} subRow row number in subcell of target cell
+ * @param {number} subCol column number in subcell of target cell
+ * @param {number} currentPlayer player that makes the move
+ * @returns set of: row and col of target cell, subRow and subCol of subcell, player id
+ */
+function makeMove(targetCell, subRow, subCol, currentPlayer){
+  selectedField = getSubcell(targetCell,subRow, subCol);
   board = document.getElementById("board")
   const hasSignX = selectedField.querySelector(".signX");
   const hasSignO = selectedField.querySelector(".signO");
+  
+  if (targetCell.dataset.allowed === "true"){
+
+    nextAllowed = getCell(selectedField.dataset.subRow, selectedField.dataset.subCol)    
+
+    if(nextAllowed){
+      setAllAllowedFields(board,false)
+
+      nextAllowed.dataset.allowed = true;
+      nextAllowed.classList.add("highlighted");
+    }
+  else {nextAllowed = targetCell}
+
+  console.log(`Current player = ${board.currentPlayer}`)
+
   
   if (!hasSignX && !hasSignO) {
     if (currentPlayer === 1) {
@@ -69,52 +105,56 @@ function makeMove(cell, subRow, subCol, currentPlayer){
     }
     
     board.currentPlayer = currentPlayer === 1 ? 2 : 1;
+    moveData = {
+      row: parseInt(targetCell.dataset.row),
+      col: parseInt(targetCell.dataset.col),
+      subRow: parseInt(subRow),
+      subCol: parseInt(subCol),
+      currentPlayer: board.currentPlayer
+      
+    };
+    return moveData;
+  }
   
-  } else {
+  
+  else {
     console.log("The selected field already has a sign.");
+  }
+}
+  else{
+    console.log("Cannot play here")
+    return 
   }
 }
 
 
-
-function handleSubmit() {
+/**
+ * Make move button handler. Used in player vs player gamemode.
+ */
+function humanMove() {
   const board = document.getElementById("board");
   const selectedField = board.selectedField;
   const currentPlayer = board.currentPlayer;
   
-  const currentCell = getCell(selectedField.dataset.row, selectedField.dataset.col)
-  console.log(`Cell found: ${currentCell.dataset.row}, ${currentCell.dataset.col}`) // DELETE
-  console.log(`Allowed: ${currentCell.dataset.allowed}`) // DELETE
+  const targetCell = getCell(selectedField.dataset.row, selectedField.dataset.col)
 
-  if (currentCell.dataset.allowed === "true"){
-
-    console.log("YES")
-    nextAllowed = getCell(selectedField.dataset.subRow, selectedField.dataset.subCol)
-    
-
-    if(nextAllowed){
-    
-      setAllAllowedFields(board,false)
-
-    nextAllowed.dataset.allowed = true;
-    nextAllowed.classList.add("highlighted");
-  }
-  else {nextAllowed = currentCell}
-
+  const moveData = makeMove(targetCell, selectedField.dataset.subRow, selectedField.dataset.subCol, currentPlayer);
+  
   console.log(`Current player = ${board.currentPlayer}`)
-  makeMove(currentCell, selectedField.dataset.subRow, selectedField.dataset.subCol, currentPlayer)
-  board.currentPlayer = currentPlayer === 1 ? 2 : 1;
-  }
-  else{
-    console.log("Cannot play here")
-  }
+
+  passMoveToEngine(moveData);
 
 }
 
-function replaceSubCells(cell, symbol) {
-  // if (selectedField.dataset.subRow === currentCell.dataset.row && selectedField.dataset.subCol === currentCell.dataset.col){
-  //   setAllAllowedFields(board, true)  
-  // }
+/**
+ * Removes all subcells from given cell and displays sign from parameter
+ * @param {HTMLElement} moveData Dataset of player move conatining {row,col, subRow, subCol, player}
+ */
+function replaceSubCells(moveData) {
+
+  if (moveData["row"] === moveData["subRow"] && moveData["col"] === moveData["subCol"]){
+    setAllAllowedFields(board, true)
+  }
   const newCell = document.createElement("div");
   newCell.className = "sign-cell";
   newCell.dataset.row = cell.dataset.row;
@@ -123,7 +163,11 @@ function replaceSubCells(cell, symbol) {
 
   const symbolElement = document.createElement("div");
   symbolElement.className = "sign";
-  symbolElement.textContent = symbol;
+  symbolElement.textContent = "";
+  if(moveData["currentPlayer"] === 1){
+    symbolElement.textContent = "O";
+  }
+  else {symbolElement.textContent = "X"}
 
   newCell.appendChild(symbolElement);
 
@@ -132,7 +176,11 @@ function replaceSubCells(cell, symbol) {
 
 
 // HELPERS
-
+/**
+ * Sets all subcells allowed state to true or false
+ * @param {HTMLElement} board board which cells should be set
+ * @param {string} state state which will be set
+ */
 function setAllAllowedFields(board,state){
   const cells = [...board.getElementsByClassName("cell")]; 
 
@@ -142,18 +190,35 @@ function setAllAllowedFields(board,state){
     }
 }
 
+/**
+ * Implements game finish after game is won. Disables further moves
+ * @param {number} player Player that has won.
+ */
 function GameIsWon(player) {
   const gameContainer = document.getElementById("game-container");
   const moveButton = document.getElementById("move-button")
   const message = document.createElement("div");
-  message.textContent = `${player} has won!`;
+  board = document.getElementById("board")
+  gameMode = board.dataset.gameMode
+  console.log(`Player :${player} and game mode: ${gameMode} `)
+  if(gameMode === "1" && player === 2){
+    message.textContent = `CPU won!`;
+  }
+  else{
+  message.textContent = `Player${player} won!`;
+  }
   message.className = "message";
 
   moveButton.style.display = "none"
   gameContainer.parentNode.appendChild(message);
 }
 
-
+/**
+ * Getter function to return cell object from given coordinates
+ * @param {number} row row parameter of given cell
+ * @param {number} col col parameter of given cell
+ * @returns {HTMLElement} Cell of given row and col
+ */
 function getCell(row, col){
   const cells = board.getElementsByClassName("cell");
     for (let i = 0; i < cells.length; i++) {
@@ -165,9 +230,16 @@ function getCell(row, col){
     return null;
 }
 
-function getSubcell(currentCell,subRow, subCol){
+/**
+ * Getter function to return subcell object from given cell with set coordinates
+ * @param {HTMLElement} targetCell cell which should be searched
+ * @param {number} subRow row parameter of given subcell 
+ * @param {number} subCol column parameter of given subcell
+ * @returns {HTMLElement}
+ */
+function getSubcell(targetCell,subRow, subCol){
 
-  const subCells = currentCell.getElementsByClassName("sub-cell");
+  const subCells = targetCell.getElementsByClassName("sub-cell");
     for (let i = 0; i < subCells.length; i++) {
       const subCell = subCells[i];
       if (subCell.dataset.subRow === subRow.toString() && subCell.dataset.subCol === subCol.toString()) {
@@ -175,4 +247,59 @@ function getSubcell(currentCell,subRow, subCol){
       }
     }
     return null;
+}
+
+/**
+ * Gets cookie of a given name
+ * @param {string} name Cookie name
+ * @returns cookie object
+ */
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
+
+// w tej funkcji chcemy wykonany ruch przekazać do silnika i zapytać, która plansza wygrana. Specjalny kod oznacza wygraną grę
+
+/**
+ * Communicates to Django views function. Passes one of the players move
+ * @param {HTMLElement} moveData dataset representing move of a player
+ */
+function passMoveToEngine(moveData){
+
+  const csrftoken = getCookie('csrftoken')
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', '/pass_move', true);
+  xhr.setRequestHeader('Content-Type', 'application/json');
+  xhr.setRequestHeader('X-CSRFToken', csrftoken)
+  gameMode = document.getElementById("board").dataset.gameMode;
+  
+  if (moveData){
+  moveData =  JSON.stringify(moveData)
+  move = JSON.parse(moveData)
+  console.log(`MoveData: ${moveData}`)
+  
+  xhr.send(moveData);
+
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+      const response = JSON.parse(xhr.responseText)
+      console.log(response)
+      if (response["gameWon"]) {GameIsWon(move["currentPlayer"]);}
+      
+      if (response["subCellWon"]) {
+        cell = getCell(move["row"], move["col"]);
+        replaceSubCells(move);
+      }
+      
+      else if(gameMode === "1") {
+        const targetCell = getCell(response["row"], response["col"]);
+        makeMove(targetCell,response["subRow"], response["subCol"],2)}
+    
+    }
+  };
+
+
+  }
 }
--- web_interface/main_menu/views.py
+++ web_interface/main_menu/views.py
@@ -1,30 +1,76 @@
+## @file
+# Django views for the game.
+# Contains game logic and interaction with gRPC server.
+
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+import main_menu.game_pb2 as game_pb2
+import main_menu.game_pb2_grpc as game_pb2_grpc
+import grpc
 import json
-# import main_menu.game_pb2 as game_pb2
-# import main_menu.game_pb2_grpc as game_pb2_grpc
-import grpc 
-
-
-# To DO:
-# 1. Zmienić endpoint z game_options na game
-# 2.W if request == POST -> if game mode in -
-# 3. Zaimplementować zmianę tury i zapis ruchu
-  
 
 def game(request):
-    if request.method == 'POST':
-        mode = request.POST['game_mode']
-        board_size = request.POST['board_size']
-        
-        # with grpc.insecure_channel('localhost:50051') as channel:
-        #     stub = game_pb2_grpc.GameStub(channel)
-        #     Move = game_pb2.Move(row=0, col=0, subRow=0, subCol=0,sign='x')
-        #     engine_reply = stub.MakeMove(Move)
-        #     print(engine_reply)
+    """
+    Renders the game view or handles the game form submission.
 
-        return render(request, 'board.html', {'board_size': board_size})
-    else:
+    :param request: The HTTP request object.
+    :type request: django.http.HttpRequest
+    :return: The rendered response.
+    :rtype: django.http.HttpResponse
+    """
+    if request.method == 'POST':
+        game_mode = request.POST['game_mode']
+        board_size = request.POST['board_size']
+        print(f'Boardsize: {board_size}, GAMEMODE: {game_mode}')
+        setup_game(int(board_size), int(game_mode))
+        return render(request, 'board.html', {'board_size': board_size, 'game_mode': game_mode})
+    elif request.method == 'GET':
         return render(request, 'game_options.html')
 
+def pass_move(request):
+    """
+    Handles the gRPC call for passing a player's move to the engine.
 
+    :param request: The HTTP request object.
+    :type request: django.http.HttpRequest
+    :return: The JSON response.
+    :rtype: django.http.JsonResponse
+    """
+    data = json.loads(request.body)
+    row = data["row"]
+    col = data["col"]
+    subRow = data["subRow"]
+    subCol = data["subCol"]
+    player = data["currentPlayer"]
+
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = game_pb2_grpc.GameStub(channel)
+        move = game_pb2.Move(row=row, col=col, subRow=subRow, subCol=subCol, player=player)
+        engine_reply = stub.MakeMove(move)
+
+    response_data = {
+        'gameWon': engine_reply.gameWon,
+        'subCellWon': engine_reply.subCellWon,
+        'row': engine_reply.row,
+        'col': engine_reply.col,
+        'subRow': engine_reply.subRow,
+        'subCol': engine_reply.subCol
+    }
+    print(response_data)
+
+    return JsonResponse(response_data)
+
+def setup_game(board_size, game_mode):
+    """
+    Sets up the game by sending a gRPC request to the engine.
+
+    :param board_size: The size of the game board.
+    :type board_size: int
+    :param game_mode: The game mode.
+    :type game_mode: int
+    """
+    with grpc.insecure_channel('localhost:50051') as channel:
+        stub = game_pb2_grpc.GameStub(channel)
+        setup = game_pb2.Setup(boardSize=board_size, gameMode=game_mode)
+        engine_reply = stub.GameSetup(setup)
+        print("Game Setup Client Request:", engine_reply)
